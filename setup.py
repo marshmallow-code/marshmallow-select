ea5b92480b056e60e08caac58663d4f660c87855
Daniel Radetsky <dradetsky@gmail.com>
2017-05-03 18:47:01 -0700

    more setup

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
 from setuptools import setup
 
+# NOTE(dmr, 2017-05-03): These are the versions I've actually used it
+# with; no idea what actual requirements are.
+REQUIRES = (
+    'marshmallow>=2.10.3',
+    'SQLAlchemy>=1.1.6',
+)
+
 setup(
     name='marshmallow-select',
-    package='marshmallow_select'
+    package='marshmallow_select',
+    description='make sqlalchemy select fields with marshmallow schemas',
+    version='0.0.0',
+    install_requires=REQUIRES,
+    author='dmr',
+    author_email='dmr@distribute.com',
+    # NOTE(dmr, 2017-05-03): I assume/hope versions 3.{4,5} work. I've only used 6.
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
 )
