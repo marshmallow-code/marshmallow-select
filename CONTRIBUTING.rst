043d4a6a54eaa318ed6bb33afaf94b3c5f1cfa15
Daniel Radetsky <dradetsky@gmail.com>
2017-05-31 11:48:45 -0700

    fix contrib dox

--- CONTRIBUTING.rst
+++ CONTRIBUTING.rst
@@ -4,10 +4,10 @@ Contrib Guide
 Setup
 =====
 
-```shell
-python -m venv venv
-source venv/bin/activate
-pip install -r dev-requirements.txt
-pip install -e .
-invoke tests
-```
+::
+
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r dev-requirements.txt
+    pip install -e .
+    invoke tests
--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import sys
+import webbrowser
 
 from invoke import task
 
@@ -8,3 +9,18 @@ def test(ctx):
     import pytest
     retcode = pytest.main([])
     sys.exit(retcode)
+
+
+# TODO(dmr, 2017-05-31): dry out readme & contrib
+@task
+def readme(ctx, browse=True):
+    ctx.run('rst2html.py README.rst > /tmp/README.html')
+    if browse:
+        webbrowser.open_new_tab('/tmp/README.html')
+
+
+@task
+def contrib(ctx, browse=True):
+    ctx.run('rst2html.py CONTRIBUTING.rst > /tmp/CONTRIBUTING.html')
+    if browse:
+        webbrowser.open_new_tab('/tmp/CONTRIBUTING.html')
