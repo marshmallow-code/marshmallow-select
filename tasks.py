40181c65f46fc9a69356b4aa498e2cb8f4334e9d
Daniel Radetsky <dradetsky@gmail.com>
2017-06-04 19:08:14 -0700

    add tasks, pkg/pub deps

--- tasks.py
+++ tasks.py
@@ -11,6 +11,27 @@ def test(ctx):
     sys.exit(retcode)
 
 
+@task
+def build(ctx):
+    ctx.run('python setup.py sdist', echo=True)
+    ctx.run('python setup.py bdist_wheel', echo=True)
+
+
+@task
+def publish(ctx):
+    clean(ctx)
+    build(ctx)
+    ctx.run('twine upload dist/*', echo=True)
+
+
+@task
+def clean(ctx):
+    ctx.run("rm -rf build")
+    ctx.run("rm -rf dist")
+    ctx.run("rm -rf marshmallow_select.egg-info")
+    print("Cleaned up.")
+
+
 # TODO(dmr, 2017-05-31): dry out readme & contrib
 @task
 def readme(ctx, browse=True):
@@ -24,3 +45,18 @@ def contrib(ctx, browse=True):
     ctx.run('rst2html.py CONTRIBUTING.rst > /tmp/CONTRIBUTING.html')
     if browse:
         webbrowser.open_new_tab('/tmp/CONTRIBUTING.html')
+
+
+#
+# misc tasks
+#
+
+
+@task
+def build_sdist(ctx):
+    ctx.run('python setup.py sdist', echo=True)
+
+
+@task
+def build_bdist(ctx):
+    ctx.run('python setup.py bdist_wheel', echo=True)
